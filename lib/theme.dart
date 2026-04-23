cbedc627ed425141a153bf3da4232a8c9cb20a55
Diana Sari <liu.687836@gmail.com>
2026-04-23 10:49:56 +0800

    Create theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final ThemeData windowsTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xFF0078D4), // Windows 蓝
+    brightness: Brightness.light,
+  ),
+  useMaterial3: true,
+  fontFamily: 'Microsoft YaHei',
+);
